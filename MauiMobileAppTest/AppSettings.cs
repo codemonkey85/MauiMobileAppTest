16d9c9c3a08b613e0ff06f1d1d6a93a654fc9b70
Michael Bond <mbond@ltisolutions.com>
2022-02-20 17:13:51 -0500

    Add appsettings and stuff

---
+++ MauiMobileAppTest/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiMobileAppTest
+{
+    public class AppSettings
+    {
+        public IDictionary<string, string> ConnectionStrings { get; set; }
+    }
+}
+
+/*
+{
+  "ConnectionStrings": {
+    "sqlite": "Data Source=data.db"
+  },
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft.AspNetCore": "Warning"
+    }
+  }
+}
+*/
